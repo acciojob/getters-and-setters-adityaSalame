8628a32b185370377c8346ac80f6786586df8a2a
Aditya <121721549+adityaSalame@users.noreply.github.com>
2023-08-07 00:02:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -35,7 +35,8 @@ class Teacher extends Person {
 		console.log(`${this.name} is teaching`);
 	}
 }
-
+const person = new Person("John", 25);
+ const student = new Student("John", 30);
 // Do not change the code below this line
 window.Person = Person;
 window.Student = Student;
